410f21f602b8644fa27ce2a72ad1c578e44536c0
Jesse <31576321+agentjesse@users.noreply.github.com>
2017-10-05 00:19:06 -0400

    append child attempt in comments

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//n.t.s. avoid jQuery if possible
+//avoid jQuery
 function search(){
   //assign the searchField value to somewhere
   let searchValue = document.getElementById('searchField').value;
@@ -13,8 +13,16 @@ function search(){
       console.log(URL);
       console.log(response);
       for(let i = 0; i<response[3].length; i++){
-        // bad way?
+        // bad way
         document.getElementById('results').innerHTML += '<div><a target="_blank"href='+ response[3][i]+'>'+response[3][i]+'</a></div>';
+
+
+        // var newDiv = document.createElement("div");
+        // var newContent = document.createTextNode("Hi there and greetings!");
+        // newDiv.appendChild(newContent); //add the text node to the newly created div.
+        // // add the newly created element and its content into the DOM
+        // var currentDiv = document.getElementById("results");
+        // document.body.insertBefore(newDiv, currentDiv);
 
       }
 
