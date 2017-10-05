ec4851c4b7c8c87e92894d16ea60a08112a285fc
Jesse <31576321+agentjesse@users.noreply.github.com>
2017-10-04 22:34:44 -0400

    get request working

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+//avoid jQuery
 function search(){
-  alert('dsfds');
+  //assign the searchField value to somewhere
+  let searchValue = document.getElementById('searchField').value;
+  URL = "https://en.wikipedia.org/w/api.php?action=opensearch&search="
+  + encodeURIComponent(searchValue) + "&format=json";
+
+  $.ajax({
+    url: URL,
+    type: 'GET',
+    //... random solution, change dataType to jsonp instead of json....fuck
+    dataType: 'jsonp',
+    success(response){
+      console.log(response);
+    },
+    error(jqXHR,status,errorThrown){
+      console.log( jqXHR );
+    }
+  });
+
+
 }
