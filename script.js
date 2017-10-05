f1d6b3db01692f1a30d8255334ef2a199482b435
Jesse <31576321+agentjesse@users.noreply.github.com>
2017-10-05 00:43:56 -0400

    append child working

--- script.js
+++ script.js
@@ -13,16 +13,17 @@ function search(){
       console.log(URL);
       console.log(response);
       for(let i = 0; i<response[3].length; i++){
-        // // bad way
+        // // bad way , resort back to this if append child is not possible yet
         // document.getElementById('results').innerHTML += '<div><a target="_blank"href='+ response[3][i]+'>'+response[3][i]+'</a></div>';
 
 
-        var newDiv = document.createElement("div");
-        var newContent = document.createTextNode("Hi there and greetings!");
-        newDiv.appendChild(newContent); //add the text node to the newly created div.
+        let newDiv = document.createElement("div");
+        newDiv.innerHTML = '<div><a target="_blank"href='+ response[3][i]+'>'+response[3][i]+'</a></div>';
+        // let newContent = document.createTextNode("Hi there and greetings!");
+        // newDiv.appendChild(newContent); //add the text node to the newly created div.
         // add the newly created element and its content into the DOM
-        var currentDiv = document.getElementById("results");
-        document.body.appendChild(newDiv);
+        let currentDiv = document.getElementById("results");
+        currentDiv.appendChild(newDiv);
 
       }
 
